043f2ce4d61947111b9aa5223f75e33d209121c6
0tiy <x.tiy@pm.me>
2024-12-01 10:10:45 +0100

    coded schedule essentials

--- src/main/java/com/example/AttendanceApp/controllers/ScheduleController.java
+++ src/main/java/com/example/AttendanceApp/controllers/ScheduleController.java
@@ -1,14 +1,45 @@
 package com.example.AttendanceApp.controllers;
 
+import com.example.AttendanceApp.models.Schedule;
+import com.example.AttendanceApp.services.ScheduleService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDateTime;
+
+@Controller
 public class ScheduleController {
-    //scheduleService
 
-    @GetMapping("/")
+    private final ScheduleService scheduleService;
+
+    public ScheduleController(ScheduleService scheduleService) {
+        this.scheduleService = scheduleService;
+    }
+
+    @GetMapping("/schedule")
     public String getMainPage(Model model){
-        //get Schedule
-        return "index";
+        model.addAttribute("schedule", scheduleService.getSchedule());
+        return "schedule";
+    }
+
+    @GetMapping("/add-schedule")
+    public String getScheduleForm() {
+        return "add-schedule";
+    }
+
+    @PostMapping("/add-schedule")
+    public String createSchedule(@RequestParam long employeeId,
+                                 @RequestParam LocalDateTime shiftStart,
+                                 @RequestParam LocalDateTime shiftEnd,
+                                 @RequestParam Double workedHours,
+                                 @RequestParam boolean isPresent) {
+        Schedule schedule = new Schedule(employeeId, shiftStart, shiftEnd, workedHours, isPresent);
+        if (scheduleService.isExist(employeeId, shiftStart, shiftEnd, workedHours, isPresent)) {
+            scheduleService.saveSchedule(schedule);
+        }
+        return "redirect:/schedule";
     }
 }
---
+++ src/main/java/com/example/AttendanceApp/services/ScheduleService.java
@@ -0,0 +1,20 @@
+package com.example.AttendanceApp.services;
+
+import com.example.AttendanceApp.models.Schedule;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface ScheduleService {
+    boolean isExist(long employeeId, LocalDateTime shiftStart, LocalDateTime shiftEnd, Double workedHours, boolean isPresent);
+
+    List<Schedule> getSchedule();
+
+    Schedule getScheduleById(long id);
+
+    void saveSchedule(Schedule schedule);
+
+    void deleteSchedule(Schedule schedule);
+
+    void updateScheduleById(long id);
+}
