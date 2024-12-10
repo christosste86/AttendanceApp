50ae637923ed9f270936251805e2ebe1f6e5783e
I have an idea <49106207+christosste86@users.noreply.github.com>
2024-12-10 17:28:36 +0100

    add select month and create date line

--- src/main/java/com/example/AttendanceApp/controllers/ScheduleController.java
+++ src/main/java/com/example/AttendanceApp/controllers/ScheduleController.java
@@ -8,12 +8,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
+    private LocalDate selectedMonth;
+    private List<LocalDate> days = new ArrayList<>();
 
     public ScheduleController(ScheduleService scheduleService) {
         this.scheduleService = scheduleService;
@@ -22,6 +28,8 @@ public class ScheduleController {
     @GetMapping("/schedule")
     public String getMainPage(Model model){
         model.addAttribute("schedule", scheduleService.getSchedule());
+        model.addAttribute("daysOfMonth", this.days);
+        model.addAttribute("selectedMonth", this.selectedMonth);
         return "schedule";
     }
 
@@ -48,6 +56,22 @@ public class ScheduleController {
 //            System.out.println("Adding schedule");
 //            scheduleService.saveSchedule(schedule);
 //        }
+        return "redirect:/schedule";
+    }
+
+
+    @GetMapping("/month-list/")
+    public String showMonthList(@RequestParam(value = "month", required = false) String selectedMonth, Model model) {
+        System.out.println("Received String: " + selectedMonth);
+        this.selectedMonth = LocalDate.parse(selectedMonth+"-01");
+        System.out.println("Received LocalDate: " + this.selectedMonth);
+        this.days.clear();
+        for(int day = 0; day < this.selectedMonth.lengthOfMonth(); day++){
+            if(this.selectedMonth != null){
+                this.days.add(this.selectedMonth.plusDays(day));
+                System.out.println(this.selectedMonth.plusDays(day));
+            }
+        }
         return "redirect:/schedule";
     }
 }
